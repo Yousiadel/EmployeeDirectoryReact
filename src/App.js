e38010869ff75c3baea73e8e28458305b82e37c6
Youstina Ibrahim <youstinaaibrahim@gmail.com>
2020-08-11 20:30:17 -0400

    EmployeeReact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React, { Component } from 'react';
-import Home from './utils/Home';
-import Header from './components/Header';
-import Container from "./components/Container";
-
-
+import React from "react";
+import SearchResultContainer from "./components/SearchResultContainer";
+import SearchForm from "./components/SearchForm";
+import ResultList from "./components/ResultList";
+import EmployeeCard from "./components/EmployeeCard";
+import axios from "axios";
 function App() {
-
-
-    return (
-
-        <div>
-            <Header />
-            <Container>
-                <Home />
-            </Container>
-        </div>
-
-    );
-
-
+  return <SearchResultContainer />;
 }
 
 export default App;
