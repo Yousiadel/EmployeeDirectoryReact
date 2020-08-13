f89cb7458961642d54a153b3d8d1d8d9ed26eb52
Youstina Ibrahim <youstinaaibrahim@gmail.com>
2020-08-13 11:20:33 -0400

    Updates

---
+++ precache-manifest.8bbb0471cb79353dbb921e8318533e42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0842fde02b2db03e5510b218a28446a",
+    "url": "/Yousiadel/EmployeeDirectoryReact/index.html"
+  },
+  {
+    "revision": "07ae00932383cde542dd",
+    "url": "/Yousiadel/EmployeeDirectoryReact/static/css/main.80e25bae.chunk.css"
+  },
+  {
+    "revision": "2e8577f6067b8e14d16b",
+    "url": "/Yousiadel/EmployeeDirectoryReact/static/js/2.c1e8bf86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Yousiadel/EmployeeDirectoryReact/static/js/2.c1e8bf86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ae00932383cde542dd",
+    "url": "/Yousiadel/EmployeeDirectoryReact/static/js/main.ec17d992.chunk.js"
+  },
+  {
+    "revision": "66aac791864901fe887c",
+    "url": "/Yousiadel/EmployeeDirectoryReact/static/js/runtime-main.27ed24db.js"
+  }
+]);
